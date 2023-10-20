caa17c6dd54c5f34771d2058ca1bb894fefb1ee5
shofegen <danik_lik1@mail.ru>
2023-10-20 16:48:03 +0500

    My homework is done

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private String nameBook;
+    private String author;
+    private int yearOfPublication;
+
+    public Book(String nameBook, String author, int yearOfPublication) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+    public String getNameBook() {
+        return this.nameBook;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public int getYearOfPublication() {
+        return this.yearOfPublication;
+    }
+
+    public void setYearOfPublication(int yearOfPublication) {
+        this.yearOfPublication = yearOfPublication;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author tolstoy = new Author("Лев", "Николаевич");
+        Book annaKarenina = new Book("Анна Каренина", "Толстой Лев Николаевич", 1873);
+        Author lermontov = new Author("Михаил", "Лермонтов");
+        Book mtsyri = new Book("Мцыри", "Михаил Юрьевич Лермонтов", 1837);
+        mtsyri.setYearOfPublication(1838);
+        System.out.println("tolstoy.getName() = " + tolstoy.getName());
+        System.out.println("tolstoy.getSurname() = " + tolstoy.getSurname());
+        System.out.println("annaKarenina.getNameBook() = " + annaKarenina.getNameBook());
+        System.out.println("annaKarenina.getAuthor() = " + annaKarenina.getAuthor());
+        System.out.println("annaKarenina.getYearOfPublication() = " + annaKarenina.getYearOfPublication());
+        System.out.println("lermontov.getName() = " + lermontov.getName());
+        System.out.println("lermontov.getSurname() = " + lermontov.getSurname());
+        System.out.println("mtsyri.getNameBook() = " + mtsyri.getNameBook());
+        System.out.println("mtsyri.getAuthor() = " + mtsyri.getAuthor());
+        System.out.println("mtsyri.getYearOfPublication() = " + mtsyri.getYearOfPublication());
     }
-}
+    }
