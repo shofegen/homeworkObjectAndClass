e78b04739f7322a0342a361eb005cb7c1fcd0861
shofegen <danik_lik1@mail.ru>
2023-10-24 11:04:51 +0500

    My homework is done

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private final String nameBook;
     private final Author author;
@@ -24,7 +26,21 @@ public class Book {
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
 
-
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(nameBook, book.nameBook) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, yearOfPublication);
+    }
+    public String toString() {
+        return this.nameBook + " " + this.author + " " + this.yearOfPublication;
+    }
 }
